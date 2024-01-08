a1dccf731e1e8ea6a4db23ca18ac5f93757da8c6
ned-choi <ned.choi@kakaocorp.com>
2024-01-08 22:23:32 +0900

    first commit

---
+++ src/main/java/com/example/checkercommitgithub/CheckerCommitGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkercommitgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckerCommitGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckerCommitGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/checkercommitgithub/CheckerCommitGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkercommitgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckerCommitGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
