0fea2d9851f52f2688b2f2f829009d1c57f64aa8
choiyoungkwon12 <hpwh4457@naver.com>
2024-01-12 03:22:22 +0900

    localdatetime으로 변경

--- src/main/java/com/example/checkercommitgithub/service/dto/Author.java
+++ src/main/java/com/example/checkercommitgithub/service/dto/Author.java
@@ -1,9 +1,12 @@
 package com.example.checkercommitgithub.service.dto;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Author {
     private String name;
     private String email;
-    private String date;
+    private LocalDateTime date;
 
     public String getName() {
         return name;
@@ -13,7 +16,7 @@ public class Author {
         return email;
     }
 
-    public String getDate() {
+    public LocalDateTime getDate() {
         return date;
     }
 }
