828979e3cbfde6f8c99cd27a4dde0cb62007ae00
choiyoungkwon12 <hpwh4457@naver.com>
2024-02-06 08:21:15 +0900

    삭제

--- src/main/java/com/example/checkercommitgithub/constant/repo/GithubAlgorithmRepo.java
+++ src/main/java/com/example/checkercommitgithub/constant/repo/GithubAlgorithmRepo.java
@@ -3,7 +3,6 @@ package com.example.checkercommitgithub.constant.repo;
 public enum GithubAlgorithmRepo {
     NED("choiyoungkwon12", "Algorithm-Practice"),
     JACK("KimHaKyeon95", "code_test"),
-    CLYDE("Valium-K", "algorithm"),
     DEVIN("zlarbals", "Algorithm_Practice"),
     ;
     private final String username;
