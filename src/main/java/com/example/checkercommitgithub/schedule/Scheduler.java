56fb406582625ee9f56448fdc63af078da660255
choiyoungkwon12 <hpwh4457@naver.com>
2024-03-12 02:05:36 +0900

    로그 추가

--- src/main/java/com/example/checkercommitgithub/schedule/Scheduler.java
+++ src/main/java/com/example/checkercommitgithub/schedule/Scheduler.java
@@ -1,9 +1,11 @@
 package com.example.checkercommitgithub.schedule;
 
 import com.example.checkercommitgithub.service.GithubDashboardService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class Scheduler {
     private final GithubDashboardService githubDashboardService;
@@ -12,8 +14,9 @@ public class Scheduler {
         this.githubDashboardService = githubDashboardService;
     }
 
-    @Scheduled(cron = "0 0 6 * * *")
+    @Scheduled(cron = "0 0 6 * * *", zone = "Asia/Seoul")
     public void check() {
+        log.info("---- check ----");
         githubDashboardService.checkGithubCommit();
     }
 }
